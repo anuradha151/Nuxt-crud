adffe4f842c94810ac81a141d2c774491c75367f
anuradha151 <anuranasinghe151@gmail.com>
2018-11-19 15:54:02 +0530

    initial commtit

---
+++ pages/category.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="box">
+            <div class="columns">
+                <div class="column">
+                     <p class="is-size-5">Category</p>
+                </div>                            
+                    <div class="control is-rounded has-icons-right">                       
+                        <input class="input" type="text" placeholder="Search">
+                        <span class="icon is-small is-right">
+                        <i class="fa fa-search" aria-hidden="true"></i>
+                        </span>            
+                    </div>
+                </div>
+            </div>           
+        </div>
+</template>
+<script>
+export default {
+  layout: 'cms'
+}
+</script>
+
