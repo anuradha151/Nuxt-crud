ad1d8c42bef652db0d63bea6d96972cf07274bce
anuradha151 <anuranasinghe151@gmail.com>
2018-11-19 16:07:12 +0530

    search bar settled

--- pages/category.vue
+++ pages/category.vue
@@ -4,18 +4,18 @@
             <div class="columns">
                 <div class="column">
                      <p class="is-size-5">Category</p>
-                </div>                      
-                    <div class="control is-rounded has-icons-right">      
-                        
+                </div>   
+                <div class="column is-one-quarter">
+                    <div class="control is-rounded has-icons-right">                        
                         <input class="input" type="text" placeholder="Search">
                         <span class="icon is-small is-right">
                             <font-awesome-icon icon="search"></font-awesome-icon>                 
-                        <i class="fa fa-search" aria-hidden="true"></i>
                         </span>            
                     </div>
                 </div>
             </div>           
         </div>
+    </div>
 </template>
 <script>
 export default {
