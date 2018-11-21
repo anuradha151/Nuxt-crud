8b003a6e68162689a3755dff4a2c60f018940a29
anuradha151 <anuranasinghe151@gmail.com>
2018-11-21 09:23:35 +0530

    new day. new commits

--- pages/category.vue
+++ pages/category.vue
@@ -3,7 +3,11 @@
         <div class="box">
             <div class="columns">
                 <div class="column">
-                     <p class="is-size-5">Category</p>
+                     <p class="is-size-5">
+                     <span class="icon is-small is-left">
+                          <font-awesome-icon icon="dot-circle"></font-awesome-icon>  
+                     </span>                    
+                    Category</p>
                 </div>   
                 <div class="column is-one-quarter">
                     <div class="control is-rounded has-icons-right">                        
@@ -12,9 +16,26 @@
                             <font-awesome-icon icon="search"></font-awesome-icon>                 
                         </span>            
                     </div>
+                </div>                
+            </div> 
+   <div class="is-divider" data-content="OR"></div>
+
+            <div class="field is-horizontal">
+                <div class="field-label is-normal">
+                    <p class="is-size-6">Crop ID</p>
                 </div>
-            </div>           
-        </div>
+                <div class="field-body">
+                    <div class="control">
+                        <div class="select">
+                            <select>
+                            <option>Select dropdown</option>
+                            <option>With options</option>
+                            </select>
+                        </div>
+                    </div>                   
+                </div>
+            </div>                                 
+        </div>        
     </div>
 </template>
 <script>
@@ -22,4 +43,7 @@ export default {
   layout: 'cms'
 }
 </script>
+
+<style>
+</style>
 
