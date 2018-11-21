b3c8fc14222a3df6905b0be4d17a5394f305a4f5
anuradha151 <anuranasinghe151@gmail.com>
2018-11-21 10:05:47 +0530

    line separator added

--- pages/category.vue
+++ pages/category.vue
@@ -18,43 +18,46 @@
                     </div>
                 </div>                
             </div> 
-            <div class="is-divider" data-content="OR"></div>
-            <div class="field is-horizontal">
-                <div class="field-label is-normal">
-                    <p class="is-size-6">Crop ID</p>
+            <hr>  
+            <div class="crop-id-selector">
+                <div class="field is-horizontal">
+                    <div class="field-label is-normal">
+                        <p class="is-size-6">Crop ID</p>
+                    </div>
+                    <div class="field-body">
+                        <div class="control">   
+                            <div class="select">                        
+                                <select>
+                                <option>Select dropdown</option>
+                                <option>With options</option>
+                                </select>
+                            </div>
+                        </div>                   
+                    </div>
                 </div>
-                <div class="field-body">
-                    <div class="control">   
-                        <div class="select">                        
-                            <select>
-                            <option>Select dropdown</option>
-                            <option>With options</option>
-                            </select>
-                        </div>
-                    </div>                   
-                </div>
-            </div>                                 
+            </div>                                           
         </div> 
         <div class="field-section">
             <div class="field">
-                <label class="label">Name</label>
+                <label class="label">Name</label>                  
                 <div class="control">
                     <input class="input" type="text" placeholder="e.g Alex Smith">
                 </div>
-            </div>
+            </div>  
+                    
             <div class="field">
                 <label class="label">Email</label>
                 <div class="control">
                     <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
                 </div>
             </div>       
-        </div>        
+        </div>                
     </div>
 </template>
 <script>
 export default {
   layout: 'cms'
-}``
+}
 </script>
 
 <style>
@@ -64,5 +67,6 @@ export default {
 .field-section {
   width: 400px;
 }
+
 </style>
 
