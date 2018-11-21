bc16ef06a5d9c77b68e3ff4526ccfa21e6cb39ba
anuradha151 <anuranasinghe151@gmail.com>
2018-11-21 09:38:02 +0530

    field set added

--- pages/category.vue
+++ pages/category.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="category-main">
         <div class="box">
             <div class="columns">
                 <div class="column">
@@ -18,15 +18,14 @@
                     </div>
                 </div>                
             </div> 
-   <div class="is-divider" data-content="OR"></div>
-
+            <div class="is-divider" data-content="OR"></div>
             <div class="field is-horizontal">
                 <div class="field-label is-normal">
                     <p class="is-size-6">Crop ID</p>
                 </div>
                 <div class="field-body">
-                    <div class="control">
-                        <div class="select">
+                    <div class="control">   
+                        <div class="select">                        
                             <select>
                             <option>Select dropdown</option>
                             <option>With options</option>
@@ -35,6 +34,20 @@
                     </div>                   
                 </div>
             </div>                                 
+        </div> 
+        <div class="field-section">
+            <div class="field">
+                <label class="label">Name</label>
+                <div class="control">
+                    <input class="input" type="text" placeholder="e.g Alex Smith">
+                </div>
+            </div>
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input class="input" type="email" placeholder="e.g. alexsmith@gmail.com">
+                </div>
+            </div>       
         </div>        
     </div>
 </template>
@@ -45,5 +58,11 @@ export default {
 </script>
 
 <style>
+.category-main {
+  margin-right: 1rem;
+}
+.field-section {
+  width: 400px;
+}
 </style>
 
