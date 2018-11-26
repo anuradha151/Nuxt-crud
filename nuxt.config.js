8c85d148ed6bc27ef68fc5957e93fee75fe5be0c
anuradha151 <anuranasinghe151@gmail.com>
2018-11-26 08:46:38 +0530

    customer changes commited

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ module.exports = {
     ** You can extend webpack config here
     */
     extend(config, ctx) {
-      
+
     }
   }
 }
--- pages/customer.vue
+++ pages/customer.vue
@@ -1,13 +1,142 @@
 <template>
+  <div>
+    <div class="box">
+      <div class="field">
+        <label class="label">Name</label>
+        <div class="control">
+          <input class="input" id="txtName" type="text" placeholder="e.g Alex Smith">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Address</label>
+        <div class="control">
+          <input class="input" id="txtAddress" type="text" placeholder="e.g Galle">
+        </div>
+      </div>
+      <div class="field">
+        <label class="label">Email</label>
+        <div class="control">
+          <input class="input" id="txtEmail" type="text" placeholder="e.g. alexsmith@gmail.com">
+        </div>
+      </div>
+      <div class="field is-horizontal">
+        <div class="field-body">
+          <div class="field">
+            <label class="label">N.I.C</label>
+            <div class="control">
+              <input class="input" id="txtNic" type="text" placeholder="e.g 951561630v">
+            </div>
+          </div>
+          <div class="field">
+            <label class="label">Passport ID</label>
+            <div class="control">
+              <input class="input" id="txtPassportId" type="text" placeholder="e.g 11421414124">
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <p class="control">
+          <a v-on:click="save('hi')" id="btnSave" class="button is-primary">
+            Submit
+          </a>
+        </p>
+        <p class="control">
+          <a class="button is-light">
+            Cancel
+          </a>
+        </p>
+      </div>
+    </div>
+    <div class="customer-table">
+      <table class="table is-striped is-hoverable">
+        <tr>
+          <th>Customer ID</th>
+          <th>Name</th>
+          <th>Address</th>
+          <th>E-mail</th>
+          <th>N.I.C</th>
+          <th>Passport ID</th>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
+        <tr>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+          <td>asdkjashdjaskjd</td>
+        </tr>
 
+      </table>
+    </div>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'customer'
+    name: 'customer',
+    layout: 'cms',
+    methods: {
+      save: function(message) {
+        alert(message)
+      }
+    }
   }
+
 </script>
 
 <style scoped>
+  .box {
+    margin-right: 1rem;
+    width: 50%;
 
+  }
 </style>
